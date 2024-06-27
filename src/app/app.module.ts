38248a0fb314fcf3377a4c3f4db7c87f08a020ff
javier.virseda@izertis.com <javier.virseda@izertis.com>
2024-06-27 10:48:04 +0200

    Add AppRoutingModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
 
 
 import { HeroesModule  } from './heroes/heroes.module';
@@ -18,7 +17,6 @@ import { AppComponent } from './app.component';
     AppComponent,
   ],
   imports: [
-    AppRoutingModule,
     BrowserModule,
     CounterModule,
     HeroesModule,
